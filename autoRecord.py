2558b19e3b2032a94f7e739faeef087fff92f0b4
dk7648 <76927860+dk7648@users.noreply.github.com>
2021-08-19 16:56:08 +0900

    move page

--- autoRecord.py
+++ autoRecord.py
@@ -25,7 +25,7 @@ login_email.send_keys('id')
 
 login_password = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/form/input[2]')
 login_password.click()
-login_password.send_keys('password')
+login_password.send_keys('pw')
 
 login_button = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/form/div[5]')
 login_button.click()
@@ -37,23 +37,64 @@ driver.find_element_by_link_text('마이페이지').click()
 time.sleep(1)
 driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div/div[2]/div[2]/div').click()
 
-time.sleep(3)
+time.sleep(1)
+
+
 
 ############################################################
 # 버튼 캡처
+# time.sleep(5)
 # print(pg.position())
-# pg.screenshot('play.png',region=(35,668,60,35))
+# pg.screenshot('stop.png',region=(30,667,60,35))
 
+play_button_position = False
 #동영상 초기화
-play_button_position = pg.center(pg.locateOnScreen('play.png'))
-pg.click(play_button_position)
-pg.press(['left']*100)
+# while True:
+#     time.sleep(4)
+#     if play_button_position == False:
+#         play_button_position = pg.center(pg.locateOnScreen('play.png'))
+#     pg.leftClick(play_button_position)
+#     pg.press(['left']*30)
+#
+#     pg.moveTo(35, 667)
+#     pg.moveTo(35, 550)
+#
+#     print("실행중 : ",pg.locateOnScreen('stop.png'))
+#     pg.moveTo(35, 560)
+#
+#     print("정지중 : ", pg.locateOnScreen('play.png'))
+#     if pg.locateOnScreen('check.png') == None:
+#         pg.leftClick(play_button_position)
+#         print("restart")
+#     pg.moveTo(60, 360)
+#
+#
+#
+#     while pg.locateOnScreen('next.png') == None:
+#         time.sleep(3)
+#         print(pg.locateOnScreen('next.png'))
+#
+#     pg.click(pg.center(pg.locateOnScreen('next.png')))
 
-while pg.locateOnScreen('next_button.png') == None:
+while True:
+    driver.implicitly_wait(time_to_wait=5)
+
+    uppertag = driver.find_element_by_xpath('//*[@id="__next"]/div[2]/div[3]/div[1]/div[2]/div')
+    iframes = driver.find_element_by_tag_name('iframe')
+    driver.switch_to.frame(iframes)
     time.sleep(3)
-    print(pg.locateOnScreen('next_button.png'))
+    runbtn = driver.find_element_by_xpath('//*[@id="player"]/div[7]/div[3]/button')
+    runbtn.click()
 
-pg.click(pg.center(pg.locateOnScreen('next_button.png')))
+    pg.press(['left']*30)
+    pg.press(['right'])
+    pg.press(['left'])
+    pg.press(['space']*2)
+    while pg.locateOnScreen('next.png') == None:
+        time.sleep(3)
+        print(pg.locateOnScreen('next.png'))
+
+    pg.click(pg.center(pg.locateOnScreen('next.png')))
 
 
 # pg.leftClick(play_button_position)
