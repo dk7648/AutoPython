edc11a81df43e45887177f5c9a6424bcefa76acf
dk7648 <76927860+dk7648@users.noreply.github.com>
2021-08-19 15:14:42 +0900

    first commit

--- autoRecord.py
+++ autoRecord.py
@@ -21,11 +21,11 @@ driver.implicitly_wait(time_to_wait=5)
 #로그인
 login_email = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/form/input[1]')
 login_email.click()
-login_email.send_keys('dk7648@korea.ac.kr')
+login_email.send_keys('id')
 
 login_password = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/form/input[2]')
 login_password.click()
-login_password.send_keys('@qjackd8748')
+login_password.send_keys('password')
 
 login_button = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/form/div[5]')
 login_button.click()
@@ -40,11 +40,23 @@ driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div/div[2]/div[2]/div
 time.sleep(3)
 
 ############################################################
+# 버튼 캡처
+# print(pg.position())
+# pg.screenshot('play.png',region=(35,668,60,35))
+
 #동영상 초기화
-print(pg.locateOnScreen(r'C:\Users\qjack\Desktop\play_button.PNG'))
-print('user pg')
-position = pg.position()
-pg.click(position)
-pg.press(['left']*30)
+play_button_position = pg.center(pg.locateOnScreen('play.png'))
+pg.click(play_button_position)
+pg.press(['left']*100)
+
+while pg.locateOnScreen('next_button.png') == None:
+    time.sleep(3)
+    print(pg.locateOnScreen('next_button.png'))
+
+pg.click(pg.center(pg.locateOnScreen('next_button.png')))
 
 
+# pg.leftClick(play_button_position)
+# print('user pg')
+# position = pg.position()
+# pg.click(position)
